877625b296a69cacc75811327bf2d23875d6c64e
Daniel Young <danyoung@utexas.edu>
2024-06-11 15:42:30 -0700

    Fixed seeding

--- seeding/train_seeds.py
+++ seeding/train_seeds.py
@@ -6,7 +6,7 @@ from aquacrop.utils import prepare_weather, get_filepath
 
 from evolution.candidate import LSTMPrescriptor
 
-def train_seed(seed_path: Path, max_irr_label: float):
+def train_seed(seed_path: Path, label: list[float]):
 
     path = get_filepath('champion_climate.txt')
     wdf = prepare_weather(path)
@@ -14,8 +14,9 @@ def train_seed(seed_path: Path, max_irr_label: float):
     context_cols = ["MinTemp", "MaxTemp", "Precipitation", "ReferenceET"]
     torch_wdf = torch.tensor(wdf[context_cols].values, dtype=torch.float32, device="mps")
 
-    label = torch.tensor([max_irr_label], dtype=torch.float32, device="mps")
-    model = LSTMPrescriptor().to("mps")
+    label_tensor = torch.tensor(label, dtype=torch.float32, device="mps")
+    model = LSTMPrescriptor()
+    model.to("mps")
     model.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
     criterion = torch.nn.MSELoss()
@@ -23,16 +24,17 @@ def train_seed(seed_path: Path, max_irr_label: float):
     with tqdm(range(epochs)) as pbar:
         for epoch in pbar:
             optimizer.zero_grad()
-            _, max_irr = model(torch_wdf)
-            loss = criterion(max_irr, label)
+            smts, max_irr = model(torch_wdf)
+            output = torch.cat([smts, max_irr], dim=0)
+            loss = criterion(output, label_tensor)
             loss.backward()
             optimizer.step()
             pbar.set_description(f"Loss: {loss.item()}")
             if epoch == epochs-1:
-                print(max_irr.item())
+                print(smts, max_irr.item())
 
     torch.save(model.state_dict(), seed_path)
 
 if __name__ == "__main__":
-    train_seed(Path("seeding/seeds/0_0.pt"), 0)
-    train_seed(Path("seeding/seeds/0_1.pt"), 450)
+    train_seed(Path("seeding/seeds/0_0.pt"), [0, 0, 0, 0, 0])
+    train_seed(Path("seeding/seeds/0_1.pt"), [100, 100, 100, 100, 450])
